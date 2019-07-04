927dcb6fe9d95e2dbf46e045f7cb24d673157f9b
Raul Sposito <raulsposito@gmail.com>
2019-07-04 02:00:21 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,6 +2,7 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+  end
     elsif phrase == phrase.downcase
   return "HUH?! SPEAK UP, SONNY!"
 else return "NO, NOT SINCE 1938!"
